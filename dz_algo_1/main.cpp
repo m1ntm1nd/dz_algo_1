62388bbcb0b9a2274417bc46c6a11f99ea1d4feb
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-03-22 23:12:37 +0300

    alg1

--- dz_algo_1/main.cpp
+++ dz_algo_1/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
 #include <utility>
-
+#include <math.h>
 using namespace std;
 
 pair<int, int> solve_du(int a1, int b1, int c1, int a2, int b2, int c2) {
+	int den = b2 - b1 * a2;
+	if  (a1 == 0){
+		if ((a2 == 0) || (b1 == 0)){
+			return make_pair(-404,-404);
+		}
+		int y = c1/b1;
+		int x = (c2 - y*b2)/a2;
+		return make_pair(x,y);
+	}
+	if  (a2 == 0){
+		if ((a1 == 0) || (b2 == 0)){
+			return make_pair(-404,-404);
+		}
+		int y = c2/b2;
+		int x = (c1 - y*b1)/a1;
+		return make_pair(x,y);
+	}
+	if  (b1 == 0){
+		if ((a1 == 0) || (b2 == 0)){
+			return make_pair(-404,-404);
+		}
+		int x = c1/a1;
+		int y = (c2 - x*a2)/a1;
+		return make_pair(x,y);
+	}  
+	if  (b2 == 0){
+		if ((a2 == 0) || (b1 == 0)){
+			return make_pair(-404,-404);
+		}
+		int x = c2/a2;
+		int y = (c1 - x*a1)/a2;
+		return make_pair(x,y);
+	}    
 	int y = (c2 * a1 - a2 * c1) / (b2 - b1 * a2);
 	int x = (c1 - b1 * y) / a1;
 	return make_pair(x, y);
@@ -11,9 +44,10 @@ pair<int, int> solve_du(int a1, int b1, int c1, int a2, int b2, int c2) {
 
 int* resheto(int n) {
 	int* a = new int[n + 1];
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i <= n; i++) {
 		a[i] = i;
 	}
+	cout<<a[n]<<endl;
 	for (int i = 2; i < sqrt(n); i++) {
 		int k = i * i;
 		a[k] = 0;
@@ -35,6 +69,8 @@ int main(){
 	int n = 1000;
 	int* a = resheto(n);
 	for (int i = 2; i < n; i++) {
-		cout<<a[i]<<'\n';
+		if (a[i] != 0){
+			cout<<a[i]<<'\n';
+		}
 	}
 }
