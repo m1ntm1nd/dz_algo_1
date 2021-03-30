09036925675ecd0b1387cf21115c0b747c01ced2
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-03-30 16:07:54 +0300

    algo2

--- algotinkov/cpp/main.cpp
+++ algotinkov/cpp/main.cpp
@@ -4,9 +4,7 @@
  
  
  
-int n;
- 
-int dp[100001];
+
  
  
  
@@ -14,16 +12,7 @@ int f(int n)
  
 {
  
-  if (n == 1) return 2;
- 
-  if (n == 2) return 4;
- 
-  if (n == 3) return 7;
- 
-  if (dp[n] != -1) return dp[n];
- 
-  return dp[n] = (f(n-1) + f(n-2) + f(n-3));
- 
+ return 0;
 }
  
  
@@ -31,12 +20,16 @@ int f(int n)
 int main(void)
  
 {
+  int n,m;
+  scanf("%d%d",&n,&m);
+  int dp[n][m];
+  for (int i = 0;i<n;i++){
+    for (int j = 0;j<n;j++){
+      printf("%d",dp[i][j]);
+    }
+    printf("\n");
+  }
  
-  memset(dp,-1,sizeof(dp));
- 
-  scanf("%d",&n);
- 
-  printf("%d\n",f(n));
  
   return 0;
  
